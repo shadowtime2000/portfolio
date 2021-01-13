b3224fa6ec185eed84978b980a26a36719a200d2
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2021-01-12 16:40:53 -0800

    html minifier

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,22 @@
 const tinyCss = require("@greyskullrocks/eleventy-plugin-tinycss");
 
+const htmlmin = require("html-minifier");
+
 module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPlugin(tinyCss);
+
+    eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+        if (outputPath.endsWith(".html")) {
+            return htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true
+            });
+        } else {
+            return content;
+        }
+    });
 
     return {
         dir: {
